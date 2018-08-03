ebd45614783f4731f62cda3316bdb61a744b38a1
Arsalan Rana <arsalan.rana@ca.ibm.com>
2018-08-02 22:29:01 -0400

    added timeout to prevent dua click spam

--- src/dua.js
+++ src/dua.js
@@ -12,27 +12,31 @@ export default class Dua extends Component {
     this.state = {
       duas: "",
       id: "",
-      userId: ""
+      userId: "",
+      isButtonDisabled: false
     };
   }
 
   increaseDuas = () => {
     var self = this;
-    var duaRequest = new FormData();
-    duaRequest.set('nominee_id', this.state.id[0].props.children)
-    duaRequest.set('nominated_by', this.props.userId)
-    axios({
-      method: 'post',
-      url: 'https://fast-cove-41298.herokuapp.com/dua',
-      data: duaRequest,
-      config: { headers: { 'Content-Type': 'multipart/form-data' } }
-    })
-      .then(function (response) {
-        self.setState((prevState) => ({ duas: response.data.duas }))
-      })  
-      .catch(function (response) {
-        console.log(response);
-      });
+    if(!this.state.isButtonDisabled) {
+      var duaRequest = new FormData();
+      duaRequest.set('nominee_id', this.state.id[0].props.children)
+      duaRequest.set('nominated_by', this.props.userId)
+      axios({
+        method: 'post',
+        url: 'https://fast-cove-41298.herokuapp.com/dua',
+        data: duaRequest,
+        config: { headers: { 'Content-Type': 'multipart/form-data' } }
+      })
+        .then(function (response) {
+          self.setState((prevState) => ({ duas: response.data.duas, isButtonDisabled: true }))
+          setTimeout(() => self.setState({ isButtonDisabled: false }), 1000);
+        })  
+        .catch(function (response) {
+          console.log(response);
+        }); 
+    }
   }
 
   sleep(time) {
