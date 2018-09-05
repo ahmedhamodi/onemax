5c395dca82bc99370b6460bc9bf14aa287bd84f4
ahmedhamodi <ahmed.r.hamodi@gmail.com>
2018-09-04 23:54:00 -0400

    Updated landing page gif.

--- src/home.js
+++ src/home.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Nominees from './nominee.js';
 import { toast } from 'react-toastify';
 import { Jumbotron, Button, Panel, Glyphicon } from 'react-bootstrap';
-import collage from './images/collage_of_words.gif';
+import collage from './images/landing_page_video.gif';
 
 class Home extends Component {
   constructor(props) {
@@ -30,7 +30,7 @@ class Home extends Component {
             <Panel.Body>
               <Jumbotron style={{ backgroundColor: "white" }}>
                 <p><img width="100%" alt="900x500" src={collage} /></p>
-                <h2><br/><br/><b><em>Who is Your Local Muslim Hero?</em></b></h2>
+                <h2><br/><b><em>Who is Your Local Muslim Hero?</em></b></h2>
                 <p><br/>Across the world, Muslims are doing brilliant and leading edge work, while also being compassionate human beings. We want to showcase the small and big acts Muslims are doing to make their community a better place! Help us recognize and celebrate your local Muslim hero, and let's create a culture of positivity around Muslims trying to make a difference.</p>
                 <p><br/><b>Do you know a Muslim in your community you want to recognize?</b></p> 
                 <p>1. Upload a picture of the individual.</p>
