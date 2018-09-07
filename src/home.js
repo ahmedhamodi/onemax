8f1b6c965175c815d6682dd8465ca2101d0b054e
ahmedhamodi <ahmed.r.hamodi@gmail.com>
2018-09-07 18:30:17 -0400

    Finished redesigning the landing page UI.

--- src/home.js
+++ src/home.js
@@ -3,6 +3,11 @@ import Nominees from './nominee.js';
 import { toast } from 'react-toastify';
 import { Jumbotron, Button, Panel, Glyphicon } from 'react-bootstrap';
 import collage from './images/landing_page_video.gif';
+import nominate_vote from './images/nominate_or_vote.png';
+import dua_hand from './images/dua-hands.png';
+import blurb from './images/blurb.png';
+import camera from './images/camera.png';
+import share from './images/share.png';
 
 class Home extends Component {
   constructor(props) {
@@ -58,19 +63,22 @@ class Home extends Component {
                   }}>
                     <br/><b>Get Started</b>
                   </h2>
-                  <p style={{
-                    'color': '#015C89',
-                    'font-size': '20px',
-                  }}>1. Upload a picture of the individual.</p>
-                  <p style={{
-                    'color': '#015C89',
-                    'font-size': '20px'
-                  }}>2. Write a short blurb on what they are doing.</p>
-                  <p style={{
-                    'color': '#015C89',
-                    'font-size': '20px',
-                    paddingBottom: '20px'
-                  }}>3. Share for the world to see!</p>
+                    <img className="landing_page_icon" src={camera} alt="logo" />
+                    <img className="landing_page_icon" src={blurb} alt="logo" />
+                    <img className="landing_page_icon" src={share} alt="logo" />
+                    <p></p>
+                    <p className="landing_page_icon_text">Upload A Picture</p>
+                    <p className="landing_page_icon_text">Write Description</p>
+                    <p className="landing_page_icon_text">Share To Everyone</p>
+                    <p style={{
+                      'color': '#015C89',
+                      'font-size': '20px',
+                      paddingBottom: '30px',
+                      paddingLeft: '75px',
+                      paddingRight: '75px',
+                      paddingTop: '20px'
+                    }}>To get started using this site, please follow the above three steps! Click on 'submit a nomination' in the navigation bar at the top of the page, and begin the process. Uploading a picture, write a description, and fill in the remaining information on the submission form and submit the individual for the world to see!</p>
+                  
                 </div>
                 <div className="gray-background">
                   <h2 style={{
@@ -79,14 +87,25 @@ class Home extends Component {
                   }}>
                     <br/><b>Recognition</b>
                   </h2>
+                  <p><img style={{
+                    width: '100px',
+                    height: '100px',
+                    float: 'right',
+                    marginRight: '75px'
+                  }} width="50%" alt="200x200" src={dua_hand} /></p>
                   <p style={{
                     'color': '#015C89',
                     'font-size': '20px',
                     'border-bottom': 'solid 3px #015C89',
                     paddingBottom: '20px',
                     paddingLeft: '75px',
-                    paddingRight: '75px'
+                    paddingRight: '175px'
                   }}>You can show your support by giving duas (prayers) to these Muslim individuals. Under each nominee there is a dua button which illustrates your support for them. It indicates that you are giving them duas (prayers) for their actions and their continued success in the future.</p>
+                </div>
+                <div className="white-background">
+                  <p><img style={{
+                    padding: '75px'
+                  }} width="100%" alt="900x500" src={nominate_vote} /></p>
                 </div>
               </Jumbotron>
             </Panel.Body>
