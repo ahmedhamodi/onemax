33d19c389d38c281df268f0e913f070d3c77240f
ahmedhamodi <ahmed.r.hamodi@gmail.com>
2018-09-07 10:37:43 -0400

    Updating the landing page colour scheme and divided the sections up accordingly.

--- src/home.js
+++ src/home.js
@@ -28,16 +28,66 @@ class Home extends Component {
         }}>
           <Panel.Collapse>
             <Panel.Body>
-              <Jumbotron style={{ backgroundColor: "white" }}>
-                <p><img width="100%" alt="900x500" src={collage} /></p>
-                <h2><br/><b><em>Who is Your Local Muslim Hero?</em></b></h2>
-                <p><br/>Across the world, Muslims are doing brilliant and leading edge work, while also being compassionate human beings. We want to showcase the small and big acts Muslims are doing to make their community a better place! Help us recognize and celebrate your local Muslim hero, and let's create a culture of positivity around Muslims trying to make a difference.</p>
-                <p><br/><b>Do you know a Muslim in your community you want to recognize?</b></p> 
-                <p>1. Upload a picture of the individual.</p>
-                <p>2. Write a short blurb on what they are doing.</p>
-                <p>3. Share for the world to see!</p>
-                <p><br/><b>How to support these Heroes?</b></p>
-                <p>You can show your support by giving duas (prayers) to these Muslim individuals. Under each nominee there is a dua button which illustrates your support for them. It indicates that you are giving them duas (prayers) for their actions and their continued success in the future.</p>
+              <Jumbotron style={{
+                backgroundColor: "white",
+                paddingBottom: '0px',
+                marginBottom: '0px'
+              }}>
+                <p><img style={{ 'paddingBottom': '20px' }} width="100%" alt="900x500" src={collage} /></p>
+                <div className="gray-background">
+                  <h2 style={{
+                    'border-top': 'solid 3px #015C89',
+                    'color': '#015C89'
+                  }}>
+                    <br/><b>What is ONEMAX?</b>
+                  </h2>
+                  <p style={{
+                    'color': '#015C89',
+                    'font-size': '20px',
+                    paddingBottom: '20px',
+                    paddingLeft: '75px',
+                    paddingRight: '75px'
+                  }}>
+                    Across the world, Muslims are doing brilliant and leading edge work, while also being compassionate human beings. We want to showcase the small and big acts Muslims are doing to make their community a better place! Help us recognize and celebrate your local Muslim hero, and let's create a culture of positivity around Muslims trying to make a difference.
+                  </p>
+                </div>
+                <div className="white-background">
+                  <h2 style={{
+                    'border-top': 'solid 3px #015C89',
+                    'color': '#015C89'
+                  }}>
+                    <br/><b>Get Started</b>
+                  </h2>
+                  <p style={{
+                    'color': '#015C89',
+                    'font-size': '20px',
+                  }}>1. Upload a picture of the individual.</p>
+                  <p style={{
+                    'color': '#015C89',
+                    'font-size': '20px'
+                  }}>2. Write a short blurb on what they are doing.</p>
+                  <p style={{
+                    'color': '#015C89',
+                    'font-size': '20px',
+                    paddingBottom: '20px'
+                  }}>3. Share for the world to see!</p>
+                </div>
+                <div className="gray-background">
+                  <h2 style={{
+                    'border-top': 'solid 3px #015C89',
+                    'color': '#015C89'
+                  }}>
+                    <br/><b>Recognition</b>
+                  </h2>
+                  <p style={{
+                    'color': '#015C89',
+                    'font-size': '20px',
+                    'border-bottom': 'solid 3px #015C89',
+                    paddingBottom: '20px',
+                    paddingLeft: '75px',
+                    paddingRight: '75px'
+                  }}>You can show your support by giving duas (prayers) to these Muslim individuals. Under each nominee there is a dua button which illustrates your support for them. It indicates that you are giving them duas (prayers) for their actions and their continued success in the future.</p>
+                </div>
               </Jumbotron>
             </Panel.Body>
           </Panel.Collapse>
